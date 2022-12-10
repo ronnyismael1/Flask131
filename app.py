e47cacd420bd3f0f48d6220c77fd7a8c71b8762d
brennan-tong <brennan.tong@sjsu.edu>
2022-12-09 23:56:28 -0800

    Editing register and login route

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ followers = db.Table(
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
-    password_hash = db.Column(db.String(100))
+    password = db.Column(db.String(100))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -88,11 +88,8 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<user>'.format(self.username)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
     def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password, password)
 
     def followed_posts(self):
         followed = Post.query.join(
@@ -176,14 +173,16 @@ def Login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid Credentials')
-            return redirect(url_for('Login'))
-        login_user(user)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+
+            if user is None or not user.check_password(form.password.data):
+                flash('Invalid Login Credentials')
+                return redirect(url_for('Login'))
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template('login.html', form = form)
 
 @app.route('/logout')
 def logout():
@@ -192,17 +191,25 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
     form = RegistrationForm()
+
     if form.validate_on_submit():
-        user = User(username=form.username.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
+        hashed_password = generate_password_hash(form.password.data, method = 'sha256')
+        username = form.username.data
+        password = hashed_password
+
+
+        new_register =User(username=username, password=password)
+
+        db.session.add(new_register)
+
         db.session.commit()
-        flash('Success, You are now registered!')
+
         return redirect(url_for('Login'))
-    return render_template('registration.html', title='Register', form=form)
+
+
+    return render_template('registration.html', form=form)
+
 
 @app.route('/delete/<username>')
 @login_required
